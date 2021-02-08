e0a2567480916d788c854bb3df7e3e2246105b19
cola28966 <1435858048@qq.com>
2021-02-08 21:23:24 +0800

    代理模式初始化

---
+++ src/proxy/GumballMachine.java
@@ -0,0 +1,104 @@
+package proxy;
+
+public class GumballMachine {
+
+    private State soldOutState;
+    private State noQuarterState;
+    private State hasQuarterState;
+    private State soldState;
+    private State winnerState;
+
+    private State state = soldOutState;
+    private int count = 0;
+    private String location;
+
+    public GumballMachine(String location, int count) {
+        soldOutState = new SoldOutState(this);
+        noQuarterState = new NoQuarterState(this);
+        hasQuarterState = new HasQuarterState(this);
+        soldState = new SoldState(this);
+        winnerState = new WinnerState(this);
+
+        this.count = count;
+        if (count > 0) {
+            state = noQuarterState;
+        }
+        this.location = location;
+        this.count = count;
+        this.location = location;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void insertQuarter() {
+        state.insertQuarter();
+    }
+
+    public void ejectQuarter() {
+        state.ejectQuarter();
+    }
+
+    public void turnCrank() {
+        state.turnCrank();
+        state.dispense();
+    }
+
+    void setState(State state) {
+        this.state = state;
+    }
+
+    void releaseBall() {
+        System.out.println("A gumball comes rolling out the slot...");
+        if (count != 0) {
+            count = count - 1;
+        }
+    }
+
+    public void refill(int count) {
+        this.count = count;
+        state = noQuarterState;
+    }
+
+    public State getSoldOutState() {
+        return soldOutState;
+    }
+
+    public State getNoQuarterState() {
+        return noQuarterState;
+    }
+
+    public State getHasQuarterState() {
+        return hasQuarterState;
+    }
+
+    public State getSoldState() {
+        return soldState;
+    }
+
+    public State getWinnerState() {
+        return winnerState;
+    }
+
+    public String toString() {
+        StringBuffer result = new StringBuffer();
+        result.append("\nMighty Gumball, Inc.");
+        result.append("\nJava-enabled Standing Gumball Model #2014");
+        result.append("\nInventory: " + count + " gumball");
+        if (count != 1) {
+            result.append("s");
+        }
+        result.append("\n");
+        result.append("Machine is " + state + "\n");
+        return result.toString();
+    }
+}
---
+++ src/proxy/State.java
@@ -0,0 +1,13 @@
+package proxy;
+
+public interface State {
+
+    void insertQuarter();
+
+    void ejectQuarter();
+
+    void turnCrank();
+
+    void  dispense();
+
+}
